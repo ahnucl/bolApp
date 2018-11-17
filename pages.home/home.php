0b242e37178ce91c0c7265d0e62de06a5bf1f9e1
lukkasmatteus00 <44606694+lukkasmatteus00@users.noreply.github.com>
2018-11-17 18:16:31 -0200

    Add files via upload

--- pages.home/home.php
+++ pages.home/home.php
@@ -3,114 +3,135 @@
 	<?php include_once(ROOT."template.html/begin.php");?>
 	<?php include_once(ROOT."template.navbar/navbarLogin.php");?>
 
-                          <!-- INICIO DA COLUNA DE TORNEIOS E PARTIDAS AMISTOSAS-->
-    <div class="container-fluid">
-      <div class="row" style="min-height:100vh;">
-
-        <?php include_once(ROOT."template.navbar/sidebar.html");?>
-                                <!-- FIM DA COLUNA DE TORNEIOS E PARTIDAS AMISTOSAS-->
-                                <!-- INICIO DA PARTE DE IMAGENS E INFORMAÇÕES DE JOGOS-->
-      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-       <div class="album py-5 bg-light" style="height:100%"> <br><br>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/img6.jpg"?>" height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text" align="justify">O projeto tem como objetivo servir de ferramenta para a inclusão social da mulher, tanto no futebol como na sociedade, incentivando a prática esportiva e contribuindo para a formação cidadã.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/img3.jpg"?>" height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text" align="justify">A pelada quase não tem regras: uma bola e um mínimo de dois participantes bastam para começar um jogo. O campo pode ser uma praça, um terreno baldio ou uma rua de pouco movimento.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/img5.jpg"?>"  height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text" align="justify">O futebol vem cada vez mais se tornando um esporte popular em vários países sem muita tradição neste esporte, pois o prazer de jogar futebol com liberdade é que faz o espetáculo.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/img4.jpg"?>" height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/ball.jpg"?>" height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" src="<?=LOCALPATH."image/img7.png"?>" height="200px" width="50px" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Informações</button>
-                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                    </div>
-                    <!-- <small class="text-muted">9 mins</small> -->
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <main class="px-4 py-5">
+		<div class="container col-md-12 py-1" style="height:100%;"> 
+			<!--<div class="container">-->
+			
+			<!--<div class="row rounded bg-light py-2">
+				<div class="text-center">
+					<p class="h3"> PAINEL DE CONTROLE PESSOAL </p>
+				</div>
+			</div>-->
+			
+			<div class="row text-center py-4">
+				<div class="col-md-4">
+					<div class="container rounded bg-light">
+						<div class="float-right w-50 " >
+							<center><i class="fas fa-id-card py-2" style="font-size: 60px; #color:#585858; padding-top: -10%;"></i></center>
+						</div>
+						<h4 class="text-right py-3"><strong> Meus dados</strong> </h4>
+						<hr>
+						<h5 class="text-left">Gols</h5>
+						<div class="form-row text-left">
+							<div class="form-group col-5">
+								<label>Meus gols: <strong>5</strong></label>
+							</div>
+							<div class="form-group col-5">
+								<label>Assistências: <strong>20</strong></label>
+							</div>
+						</div>
+					
+						<h5 class="text-left">Faltas</h5>
+						<div class="form-row text-left">
+							<div class="form-group col-5 ">
+								<label>Penalidades: <strong>40</strong></label>
+								
+							</div>
+							<div class="form-group col-5 ">
+								<label>Expulsões: <strong>2</strong></label>
+								
+							</div>
+						</div>
+	
+						<h5 class="text-left">Cartões</h5>
+						<div class="form-row">
+							<div class="form-group col-5 ">
+								<label>Amarelo: <strong>4</strong></label>
+								
+							</div>
+							<div class="form-group col-5 ">
+								<label>Vermelho: <strong>2</strong></label>
+								
+							</div>
+						</div>
+						<hr>
+						<br>
+						<div class="btn-group py-4">
+							<button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa fa-plus pr-2" aria-hidden="true"></i> Informações</button>
+							<!--<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="container rounded bg-light">
+						<div class="float-right w-50 " >
+							<center><i class="fas fa-shield-alt py-2" style="font-size: 60px; #color:#585858; padding-top: -10%;"></i></center>
+						</div>
+						<h4 class="text-right py-3"><strong>Times</strong></h4>
+						
+						<table class="table table-hover table-light">
+							<thead align="center">
+								<tr>
+								  <th scope="col">Nome</th>
+								  <th scope="col">Data</th>
+								  <th scope="col">Visualizar</th>
+								</tr>
+							</thead>
+							<tbody align="center">
+								<?php
+							$i=0; 
+							for($i==0;$i<=4;$i++){ ?>
+								<tr>
+									<td> Time A </td>
+									<td> 00/00/0000 </td>
+									<td><a href="#" ><i class="fa fa-eye" style="font-size: 20px;" aria-hidden="true"></i></td>
+								</tr>
+							<?php } ?>
+							</tbody>
+						</table>
+						<hr>
+						<div class="btn-group py-3">
+							<button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa fa-plus pr-2" aria-hidden="true"></i> Informações</button>
+							<!--<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+						</div>
+					</div> 
+				</div>
+				<div class="col-md-4">
+					<div class="container rounded bg-light">
+						<div class="float-right w-50 " >
+							<center><i class="fa fa-users py-2" style="font-size: 60px; #color:#585858; padding-top: -10%;"></i></center>
+						</div>
+						<h4 class="text-right py-3"><strong>Partidas</strong></h4>
+						
+						<table class="table table-hover table-light">
+							<thead align="center">
+								<tr>
+								  <th scope="col">Times</th>
+								  <th scope="col">Data</th>
+								  <th scope="col">Visualizar</th>
+								</tr>
+							</thead>
+							<tbody align="center">
+							<?php
+							$i=0; 
+							for($i==0;$i<=4;$i++){ ?>
+								<tr>
+									<td> Time A VS Time B</td>
+									<td> 00/00/0000</td>
+									<td><a href="#" ><i class="fa fa-eye" style="font-size: 20px;" aria-hidden="true"></i></td>
+								</tr>
+							<?php } ?>
+							</tbody>
+						</table>
+						<hr>
+						<div class="btn-group py-3">
+							<button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa fa-plus pr-2" aria-hidden="true"></i> Informações</button>
+							<!--<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
     </main>
-            <!-- FIM DA PARTE DE IMAGENS E INFORMAÇÕES DE JOGOS-->
-  </div>
-</div>
   <!-- <?php //include_once(__DIR__."/footer/footerPrimary.html");?> -->
   <?php include_once(ROOT."template.html/end.php");?>
